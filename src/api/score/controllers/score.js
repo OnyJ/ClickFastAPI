78c155bdc5db1ef72c2a333a6a753f6fcf1c2d18
Josselin <julien.josselin75@hotmail.fr>
2024-11-07 15:58:01 +0100

    Run yarn, delete about article author category global about, create Score

--- src/api/score/controllers/score.js
+++ src/api/score/controllers/score.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- *  category controller
+ * score controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::score.score');
